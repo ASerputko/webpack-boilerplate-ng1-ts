034a463db19e746706e50171559806439919a83b
Andrew Serputko <aserputko@gmail.com>
2016-07-13 23:17:52 +0300

    Initialize repository

--- src/app/app.directive.ts
+++
@@ -1,6 +0,0 @@
-export default function() {
-    return {
-        restrict: 'AE',
-        template: `<h1 class="red">Hi there!</h1>`
-    };
-}
--- src/application.ts
+++ src/application.ts
@@ -1,16 +1,6 @@
 /// <reference path="../typings/tsd.d.ts"/>
 import * as angular from 'angular';
-import appDirective from './app/app.directive';
-import AppService from './app/app.service';
-
 import 'assets/scss/application.scss';
 
-
 angular
-    .module('app', [])
-    .directive('app', appDirective)
-    .service('appService', AppService)
-    .run(['appService', (appService) => {
-        let data = appService.getData();
-        console.log('service data:', data);
-    }]);
+    .module('app', []);
